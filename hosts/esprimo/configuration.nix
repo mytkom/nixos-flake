348c734a36d29a5b11d4846d3103ba1b75c32193
mytkom <marek.mytkowski.mm@gmail.com>
2024-05-09 22:12:12 +0200

    add samb config

--- hosts/esprimo/configuration.nix
+++ hosts/esprimo/configuration.nix
@@ -6,10 +6,46 @@
       ./hardware-configuration.nix
     ];
 
+  services.samba = {
+    enable = true;
+    securityType = "user";
+    openFirewall = true;
+    extraConfig = ''
+      workgroup = WORKGROUP
+      server string = smbnix
+      netbios name = smbnix
+      security = user
+      hosts allow = 192.168.0. 127.0.0.1 localhost
+      hosts deny = 0.0.0.0/0
+      guest account = nobody
+      map to guest = bad user
+    '';
+    shares = {
+      private = {
+        path = "/home";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "username";
+        "force group" = "groupname";
+      };
+    };
+  };
+  
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+  };
+  
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  networking.firewall.enable = true;
+  networking.firewall.allowPing = true;
   networking.hostName = "esprimo"; # Define your hostname.
   # Pick only one of the below networking options.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
